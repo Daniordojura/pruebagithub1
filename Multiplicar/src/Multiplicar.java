5f592569120a70d4f881b6a98bdc9af98eed6d38
Daniel <Daniel@192.168.1.190>
2019-04-03 14:14:04 +0200

    Version 1 Cuenta de multiplicar

---
+++ Multiplicar/src/Multiplicar.java
@@ -0,0 +1,18 @@
+
+//Daniel Ordóñez Jurado
+
+public class Multiplicar {
+	int num1;
+	int num2;
+	
+	public Multiplicar(int n1, int n2) {
+		num1 = n1;
+		num2 = n2;
+	}
+	
+	public int multiplicar() {
+		int resultado = num1 * num2;
+		return resultado;
+	}
+}
+
